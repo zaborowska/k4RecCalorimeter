d8c33481990f9f0251d27a2d06c8cf445d60a5a0
Anna Zaborowska <anna.zaborowska@cern.ch>
2024-04-30 11:45:06 +0200

    Fix drawing macros from combined barrel-endcap file

--- RecODDCalorimeter/tests/scripts/plot_cells.py
+++ RecODDCalorimeter/tests/scripts/plot_cells.py
@@ -8,8 +8,8 @@ from matplotlib.collections import PatchCollection
 
 plt.rcParams["figure.figsize"] = [7.00, 7.0]
 plt.rcParams["figure.autolayout"] = True
-f = root.TFile("ODD_cells.root")
-myTree = f.Get("cells")
+dfr = root.RDataFrame("cells", "ODD_cells.root")
+dfr = dfr.Filter("isBarrel").AsNumpy(["r","phi","dr","dphi","z","dz"])
 columns = ["x", "y", "z", "xA","yA","zA", "xB", "yB", "zB", "xC", "yC", "zC", "xD", "yD", "zD","phi","dphi","R","dR","dz"]
 df = pd.read_csv("ECalBarrel_cellPositions.txt", skipinitialspace=True, usecols=columns)
 print("Contents in csv file:", df)
@@ -19,37 +19,38 @@ ax1.set_ylabel("y (mm)")
 ax2.set_xlabel("z (mm)")
 errorboxes_xyCart = []
 errorboxes_xyCylindr = []
-errorboxes_xyAC = []
+errorboxes_zyCart = []
+errorboxes_zyCylindr = []
+xBarrel = []
+yBarrel = []
+counter_else = 0
 for x, y, xa, ya, xc, yc in zip(df.x, df.y, df.xA, df.yA, df.xC, df.yC):
         errorboxes_xyCart.append(Polygon(((x-xc+xa,y-yc+ya),(x+xa+xc,y+ya+yc), (x+xc-xa,y+yc-ya), (x-xa-xc,y-ya-yc))))
-for event in myTree:
-        r, phi, dr, dphi = event.r, event.phi, event.dr, event.dphi
+for x, y, xa, ya, xc, yc in zip(df.z, df.y, df.zD, df.yD, df.zB, df.yB):
+        errorboxes_zyCart.append(Polygon(((x-xc+xa,y-yc+ya),(x+xa+xc,y+ya+yc), (x+xc-xa,y+yc-ya), (x-xa-xc,y-ya-yc))))
+for r, phi, dr, dphi, z, dz in zip(dfr["r"], dfr["phi"], dfr["dr"], dfr["dphi"], dfr["z"], dfr["dz"]):
         errorboxes_xyCylindr.append(Polygon((
                 ((r-0.5*dr)*cos(phi-0.5*dphi),(r-0.5*dr)*sin(phi-0.5*dphi)), ((r-0.5*dr)*cos(phi+0.5*dphi),(r-0.5*dr)*sin(phi+0.5*dphi)),
                 ((r+0.5*dr)*cos(phi+0.5*dphi),(r+0.5*dr)*sin(phi+0.5*dphi)), ((r+0.5*dr)*cos(phi-0.5*dphi),(r+0.5*dr)*sin(phi-0.5*dphi))
-                                            )))
-
-# Create patch collection with specified colour/alpha
-pc_xyCart = PatchCollection(errorboxes_xyCart, facecolor='b', alpha=0.2, edgecolor='white', linestyle="-", linewidth=1)
-pc_xyCylindr = PatchCollection(errorboxes_xyCylindr, facecolor='r', alpha=0.2, edgecolor='r', linestyle="-", linewidth=1)
-ax1.add_collection(pc_xyCart)
-ax1.add_collection(pc_xyCylindr)
-errorboxes_zyCart = []
-errorboxes_zyCylindr = []
-for x, y, xa, ya, xc, yc in zip(df.z, df.y, df.zD, df.yD, df.zB, df.yB):
-        errorboxes_zyCart.append(Polygon(((x-xc+xa,y-yc+ya),(x+xa+xc,y+ya+yc), (x+xc-xa,y+yc-ya), (x-xa-xc,y-ya-yc))))
-for entry in myTree:
-        r, phi, dr, dphi, z, dz = event.r, event.phi, event.dr, event.dphi, event.z, event.dz
+        )))
+        xBarrel.append(r*cos(phi))
+        yBarrel.append(r*sin(phi))
         ymax = max ((r+0.5*dr)*sin(phi-0.5*dphi), (r-0.5*dr)*sin(phi-0.5*dphi), (r+0.5*dr)*sin(phi+0.5*dphi), (r-0.5*dr)*sin(phi+0.5*dphi))
         ymin = min ((r+0.5*dr)*sin(phi-0.5*dphi), (r-0.5*dr)*sin(phi-0.5*dphi), (r+0.5*dr)*sin(phi+0.5*dphi), (r-0.5*dr)*sin(phi+0.5*dphi))
         xmax = max ((r+0.5*dr)*cos(phi-0.5*dphi), (r-0.5*dr)*cos(phi-0.5*dphi), (r+0.5*dr)*cos(phi+0.5*dphi), (r-0.5*dr)*cos(phi+0.5*dphi))
         xmin = min ((r+0.5*dr)*cos(phi-0.5*dphi), (r-0.5*dr)*cos(phi-0.5*dphi), (r+0.5*dr)*cos(phi+0.5*dphi), (r-0.5*dr)*cos(phi+0.5*dphi))
         errorboxes_zyCylindr.append(Polygon((
                 (z+0.5*dz,ymax), (z-0.5*dz,ymax), (z-0.5*dz,ymin), (z+0.5*dz,ymin)
-                                            )))
+        )))
+print(len(xBarrel))
+# Create patch collection with specified colour/alpha
+pc_xyCart = PatchCollection(errorboxes_xyCart, facecolor='b', alpha=0.2, edgecolor='white', linestyle="-", linewidth=1)
+pc_xyCylindr = PatchCollection(errorboxes_xyCylindr, facecolor='r', alpha=0.1, edgecolor='r', linestyle="-", linewidth=1)
+ax1.add_collection(pc_xyCart)
+ax1.add_collection(pc_xyCylindr)
 pc_zyCart = PatchCollection(errorboxes_zyCart, facecolor='b', alpha=0.2, edgecolor='white')
 pc_zyCylindr = PatchCollection(errorboxes_zyCylindr, facecolor='r', alpha=0.1, edgecolor='r')
-ax1.scatter([ev.r*cos(ev.phi) for ev in myTree], [ev.r*sin(ev.phi) for ev in myTree],color='r', marker='+')
+ax1.scatter(xBarrel, yBarrel, color='r', marker='+')
 ax2.add_collection(pc_zyCart)
 ax2.add_collection(pc_zyCylindr)
 ax2.scatter(df.z, df.y)
--- RecODDCalorimeter/tests/scripts/plot_cells_endcap.py
+++ RecODDCalorimeter/tests/scripts/plot_cells_endcap.py
@@ -8,34 +8,37 @@ from matplotlib.collections import PatchCollection
 
 plt.rcParams["figure.figsize"] = [7.00, 7.0]
 plt.rcParams["figure.autolayout"] = True
-f = root.TFile("ODD_cells.root")
-myTree = f.Get("cells")
+dfr = root.RDataFrame("cells", "ODD_cells.root")
+dfr = dfr.Filter("isBarrel==0").AsNumpy(["r","phi","dr","dphi","z","dz"])
 fig1, (ax1, ax2) = plt.subplots(1, 2,  width_ratios= (1,3), figsize=(24,7), sharey = True)
 
 ax1.set_xlabel("x (mm)")
 ax1.set_ylabel("y (mm)")
 ax2.set_xlabel("z (mm)")
 errorboxes_xyCylindr = []
-for event in myTree:
-        r, phi, dr, dphi = event.r, event.phi, event.dr, event.dphi
+errorboxes_zyCylindr = []
+xBarrel = []
+yBarrel = []
+
+for r, phi, dr, dphi, z, dz in zip(dfr["r"], dfr["phi"], dfr["dr"], dfr["dphi"], dfr["z"], dfr["dz"]):
         errorboxes_xyCylindr.append(Polygon((
                 ((r-0.5*dr)*cos(phi-0.5*dphi),(r-0.5*dr)*sin(phi-0.5*dphi)), ((r-0.5*dr)*cos(phi+0.5*dphi),(r-0.5*dr)*sin(phi+0.5*dphi)),
                 ((r+0.5*dr)*cos(phi+0.5*dphi),(r+0.5*dr)*sin(phi+0.5*dphi)), ((r+0.5*dr)*cos(phi-0.5*dphi),(r+0.5*dr)*sin(phi-0.5*dphi))
-                                            )))
-# Create patch collection with specified colour/alpha
-pc_xyCylindr = PatchCollection(errorboxes_xyCylindr, facecolor='r', alpha=0.2, edgecolor='r', linestyle="-", linewidth=1)
-ax1.add_collection(pc_xyCylindr)
-errorboxes_zyCylindr = []
-for entry in myTree:
-        r, phi, dr, dphi, z, dz = event.r, event.phi, event.dr, event.dphi, event.z, event.dz
+        )))
+        xBarrel.append(r*cos(phi))
+        yBarrel.append(r*sin(phi))
         ymax = max ((r+0.5*dr)*sin(phi-0.5*dphi), (r-0.5*dr)*sin(phi-0.5*dphi), (r+0.5*dr)*sin(phi+0.5*dphi), (r-0.5*dr)*sin(phi+0.5*dphi))
         ymin = min ((r+0.5*dr)*sin(phi-0.5*dphi), (r-0.5*dr)*sin(phi-0.5*dphi), (r+0.5*dr)*sin(phi+0.5*dphi), (r-0.5*dr)*sin(phi+0.5*dphi))
         xmax = max ((r+0.5*dr)*cos(phi-0.5*dphi), (r-0.5*dr)*cos(phi-0.5*dphi), (r+0.5*dr)*cos(phi+0.5*dphi), (r-0.5*dr)*cos(phi+0.5*dphi))
         xmin = min ((r+0.5*dr)*cos(phi-0.5*dphi), (r-0.5*dr)*cos(phi-0.5*dphi), (r+0.5*dr)*cos(phi+0.5*dphi), (r-0.5*dr)*cos(phi+0.5*dphi))
         errorboxes_zyCylindr.append(Polygon((
                 (z+0.5*dz,ymax), (z-0.5*dz,ymax), (z-0.5*dz,ymin), (z+0.5*dz,ymin)
-                                            )))
+        )))
+print(len(xBarrel))
+# Create patch collection with specified colour/alpha
+pc_xyCylindr = PatchCollection(errorboxes_xyCylindr, facecolor='r', alpha=0.2, edgecolor='r', linestyle="-", linewidth=1)
+ax1.add_collection(pc_xyCylindr)
 pc_zyCylindr = PatchCollection(errorboxes_zyCylindr, facecolor='r', alpha=0.1, edgecolor='r')
-ax1.scatter([ev.r*cos(ev.phi) for ev in myTree], [ev.r*sin(ev.phi) for ev in myTree],color='r', marker='+')
+ax1.scatter(xBarrel, yBarrel, color='r', marker='+')
 ax2.add_collection(pc_zyCylindr)
 plt.show()
